b037f94cee21cd13339bc12828036ec57f484c3d
junior brandao <edilson_brandaojunior@hotmail.com>
2023-07-11 18:36:45 -0300

    game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -24,10 +24,8 @@ public class Game {
 	private String platforms;
 	private Double score;
 	private String imgUrl;
-	
 	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
-
 	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
